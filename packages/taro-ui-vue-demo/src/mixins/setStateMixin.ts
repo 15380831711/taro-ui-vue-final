7040d88948efc4b8038b6ede2de1d8163bfec35e
你的代码就是我的代码 <3094865027@qq.com>
2022-04-27 03:34:08 +0800

    包重命名，评分组件事件传递

--- packages/taro-ui-vue-demo/src/mixins/setStateMixin.ts
+++
@@ -1,8 +0,0 @@
-import assign from 'lodash/assign'
-export default {
-  methods: {
-    setState(state) {
-      assign(this.state, state)
-    }
-  }
-}
--- packages/taro-ui-vue-demo/src/utils/common.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: pengyue
- * @Date: 2020-07-06 21:43:33
- * @LastEditTime: 2020-07-07 01:58:28
- * @LastEditors: pengyue
- * @Description:
- * @FilePath: /taro-ui-vue/packages/taro-ui-vue-demo/src/utils/common.ts
- */
